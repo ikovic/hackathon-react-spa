10ad0142ef9bb843ce7ea53f64b60b5e282e01e1
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-10-26 13:53:28 +0200

    Working boilerplate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
+class App extends React.Component {
+  render() {
+    return <h1>ITSAMEEE</h1>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
