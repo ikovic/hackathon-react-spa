132ecc8f008e9af9c2c928c1c9152a552bf9462d
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-10-26 14:44:30 +0200

    Some styling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { LocaleProvider } from 'antd';
+import { LocaleProvider, Layout, Menu } from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
-import { Layout, Menu } from 'antd';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+import Dashboard from 'pages/Dashboard';
+import Pipeline from 'pages/Pipeline';
 
 import 'antd/dist/antd.css';
+import './styles.css';
 
 const { Header, Content, Footer } = Layout;
 
@@ -13,20 +17,36 @@ class App extends React.Component {
     return (
       <Layout className="layout">
         <Header>
-          <div className="logo" />
+          <div className="logo">
+            <a href="https://seekandhit.com">
+              <img
+                className="logoImage"
+                src="https://seekandhit.com/wp-content/themes/sih-2014/img/seekandhit.svg"
+                alt="SeekandHit Internet Marketing"
+              />
+            </a>
+          </div>
           <Menu
             theme="dark"
             mode="horizontal"
-            defaultSelectedKeys={['2']}
+            defaultSelectedKeys={['1']}
             style={{ lineHeight: '64px' }}
           >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
+            <Menu.Item key="1">
+              <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link to="/pipeline">Pipeline</Link>
+            </Menu.Item>
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px' }}>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pipeline" component={Pipeline} />
+            </Switch>
+          </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>SeekandHit ©2017</Footer>
       </Layout>
@@ -35,8 +55,10 @@ class App extends React.Component {
 }
 
 ReactDOM.render(
-  <LocaleProvider locale={enUS}>
-    <App />
-  </LocaleProvider>,
+  <BrowserRouter>
+    <LocaleProvider locale={enUS}>
+      <App />
+    </LocaleProvider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
---
+++ src/pages/Pipeline/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Pipeline = () => <h1>Pipeline</h1>;
+
+export default Pipeline;
