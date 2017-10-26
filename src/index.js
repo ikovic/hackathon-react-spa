6e35a36cf3982fd378325c6bde66dde46140f6d5
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-10-26 14:05:46 +0200

    Remove junk. Basic layout

--- src/index.js
+++ src/index.js
@@ -2,12 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { LocaleProvider } from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
+import { Layout, Menu } from 'antd';
 
 import 'antd/dist/antd.css';
 
+const { Header, Content, Footer } = Layout;
+
 class App extends React.Component {
   render() {
-    return <h1>ITSAMEEE</h1>;
+    return (
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>SeekandHit ©2017</Footer>
+      </Layout>
+    );
   }
 }
 
