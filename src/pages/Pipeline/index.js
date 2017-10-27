0cbb2a745145cabd9b68e1915229ab1565ec6a66
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-10-27 17:29:59 +0200

    In progress: Pipeline editor

--- src/pages/Pipeline/index.js
+++ src/pages/Pipeline/index.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { Card, Row, Col, Progress, Button, Switch } from 'antd';
 
-const Pipeline = () => <h1>Pipeline</h1>;
+class Pipeline extends PureComponent {
+  render() {
+    return (
+      <section>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Progress percent={30} />
+          </Col>
+          <Col span={12}>
+            <Switch checkedChildren="Active" unCheckedChildren="Paused" />
+            <Button type="primary" icon="save" size="large" disabled>
+              Save
+            </Button>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={6}>
+            <Card title="Actor">Card content</Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Event">Card content</Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Target">Card content</Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Template">Card content</Card>
+          </Col>
+        </Row>
+      </section>
+    );
+  }
+}
 
 export default Pipeline;
