fb58ab696aa7b3e91f7e4bc4a084d1818f712a7f
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-10-27 19:15:48 +0200

    Basic layout for pipeline editor

--- src/pages/Pipeline/index.js
+++ src/pages/Pipeline/index.js
@@ -1,22 +1,38 @@
 import React, { PureComponent } from 'react';
-import { Card, Row, Col, Progress, Button, Switch } from 'antd';
+import { Card, Row, Col, Progress, Button, Checkbox, Input } from 'antd';
+import './styles.css';
+
+const ProgressInfo = ({ percent }) => (
+  <span className="progressWrapper">
+    <span className="progressLabel">Setup progress:</span>
+    <Progress type="circle" width={40} percent={percent} />
+  </span>
+);
 
 class Pipeline extends PureComponent {
   render() {
     return (
-      <section>
-        <Row gutter={16}>
-          <Col span={12}>
-            <Progress percent={30} />
+      <section id="pipelineEditor">
+        <Row gutter={16} className="row">
+          <Col span={12} className="controls">
+            <span>
+              <Input
+                addonBefore="Name"
+                placeholder="Pipeline Name"
+                className="title"
+                size="large"
+              />
+            </span>
+            <Checkbox className="activeCheckbox">Active</Checkbox>
           </Col>
-          <Col span={12}>
-            <Switch checkedChildren="Active" unCheckedChildren="Paused" />
+          <Col span={12} className="saveWrapper">
+            <ProgressInfo percent={25} />
             <Button type="primary" icon="save" size="large" disabled>
               Save
             </Button>
           </Col>
         </Row>
-        <Row gutter={16}>
+        <Row gutter={16} className="row">
           <Col span={6}>
             <Card title="Actor">Card content</Card>
           </Col>
