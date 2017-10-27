f67e24d4633c7b781f4a1a0da134a7093ab84928
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-10-27 17:42:09 +0200

    Oops

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -180,8 +180,6 @@ module.exports = {
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
-                        modules: true,
-                        localIdentName: "[name]__[local]___[hash:base64:5]"
                       },
                     },
                     {
