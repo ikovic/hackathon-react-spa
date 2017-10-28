006125960d9120327129bbea3150ff5466ab965f
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-10-28 12:02:39 +0200

    Add template

--- src/pages/Pipeline/TemplateModal/index.js
+++ src/pages/Pipeline/TemplateModal/index.js
@@ -1,27 +1,35 @@
 import React, { PureComponent } from 'react';
-import { Form, Input, Modal, Button, Row, Col } from 'antd';
+import { Form, Input, Mention, Modal, Button, Row, Col } from 'antd';
+import { connect } from 'react-redux';
+import * as TemplateActions from 'redux/modules/templates';
+import './styles.css';
 
 const FormItem = Form.Item;
 
-const TemplateForm = Form.create()(({ form: { getFieldDecorator }, onSubmit }) => (
+const TemplateForm = Form.create()(({ form: { getFieldDecorator }, onSubmit, actor }) => (
   <Form onSubmit={onSubmit}>
     <FormItem label="Name">{getFieldDecorator('name')(<Input />)}</FormItem>
     <FormItem label="Link">{getFieldDecorator('link')(<Input />)}</FormItem>
+    <FormItem label="Caption">{getFieldDecorator('caption')(<Input />)}</FormItem>
     <FormItem label="Message">{getFieldDecorator('message')(<Input />)}</FormItem>
-    <FormItem label="Picture">{getFieldDecorator('picture')(<Input />)}</FormItem>
+    <FormItem label="Image URL">{getFieldDecorator('picture')(<Input />)}</FormItem>
     <FormItem label="Description">
-      {getFieldDecorator('description')(<Input type="textarea" />)}
+      {getFieldDecorator('description')(
+        <Mention style={{ width: '100%', height: 100 }} suggestions={[actor.name]} multiLines />,
+      )}
     </FormItem>
   </Form>
 ));
 
 class TemplateModal extends PureComponent {
   state = { visible: false };
+
   showModal = () => {
     this.setState({
       visible: true,
     });
   };
+
   handleOk = () => {
     const form = this.form;
     form.validateFields((err, values) => {
@@ -29,21 +37,28 @@ class TemplateModal extends PureComponent {
         return;
       }
 
-      console.log('Received values of form: ', values);
+      this.props.addTemplate({
+        ...values,
+        description: Mention.toString(values.description).replace('@', ''),
+      });
       form.resetFields();
       this.setState({ visible: false });
     });
   };
+
   handleCancel = e => {
     this.setState({
       visible: false,
     });
   };
+
   render() {
+    const actor = this.props.actor;
+
     return (
       <div>
         <Button type="primary" onClick={this.showModal}>
-          New...
+          <span className="newTemplateBtn">New...</span>
         </Button>
         <Modal
           title="New Template"
@@ -53,7 +68,11 @@ class TemplateModal extends PureComponent {
         >
           <Row>
             <Col span={12}>
-              <TemplateForm onSubmit={this.handleOk} ref={form => (this.form = form)} />
+              <TemplateForm
+                onSubmit={this.handleOk}
+                actor={actor}
+                ref={form => (this.form = form)}
+              />
             </Col>
             <Col span={12}>PREVIEW</Col>
           </Row>
@@ -63,4 +82,12 @@ class TemplateModal extends PureComponent {
   }
 }
 
-export default Form.create()(TemplateModal);
+const mapStateToProps = state => ({
+  actor: state.actors.items.find(actor => state.editor.actor === actor.id),
+});
+
+const mapDispatchToProps = dispatch => ({
+  addTemplate: template => dispatch(TemplateActions.addTemplate(template)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form.create()(TemplateModal));
--- src/redux/modules/templates.js
+++ src/redux/modules/templates.js
@@ -11,8 +11,20 @@ const initialState = {
   error: null,
 };
 
+export const ADD_TEMPLATE = 'seekandhit/templates/ADD_TEMPLATE';
+
+export const addTemplate = template => ({
+  type: ADD_TEMPLATE,
+  template: { ...template, id: String(Date.now()) },
+});
+
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    case ADD_TEMPLATE:
+      return {
+        ...state,
+        items: [...state.items, action.template],
+      };
     default:
       return state;
   }
