6d18fc46b68a2d05bb60c237875947fea67ad650
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-10-28 14:37:50 +0200

    Correct template values

--- src/pages/Pipeline/TemplateModal/TemplateForm/index.js
+++ src/pages/Pipeline/TemplateModal/TemplateForm/index.js
@@ -6,25 +6,33 @@ const FormItem = Form.Item;
 const TemplateForm = ({ form: { getFieldDecorator }, onSubmit, actor }) => (
   <Form onSubmit={onSubmit}>
     <FormItem label="Name">
-      {getFieldDecorator('name', { initialValue: 'Test' })(<Input />)}
+      {getFieldDecorator('name', {
+        initialValue: 'Heineken | #ChampionTheMatch with Zlatan Ibrahimovic',
+      })(<Input />)}
     </FormItem>
     <FormItem label="Link">
-      {getFieldDecorator('link', { initialValue: 'http://somelink.com' })(<Input />)}
+      {getFieldDecorator('link', { initialValue: 'https://www.heineken.com' })(<Input />)}
     </FormItem>
     <FormItem label="Image URL">
       {getFieldDecorator('picture', {
-        initialValue: 'http://cleantechnica.com/files/2009/07/cow.jpg',
+        initialValue:
+          'https://scontent.xx.fbcdn.net/v/t45.1600-4/22886733_6080390450050_2787285113400983552_n.png?oh=ca809ff33ea1ca7055257405c9f9694c&oe=5A6938E3',
       })(<Input />)}
     </FormItem>
     <FormItem label="Caption">
-      {getFieldDecorator('caption', { initialValue: 'A picture of a Cow' })(<Input />)}
+      {getFieldDecorator('caption', { initialValue: 'www.heineken.com' })(<Input />)}
     </FormItem>
     <FormItem label="Description">
-      {getFieldDecorator('description', { initialValue: 'Short description' })(<Input />)}
+      {getFieldDecorator('description', {
+        initialValue:
+          'Heineken® launches new UEFA Champions League campaign, starring Zlatan Ibrahimovic',
+      })(<Input />)}
     </FormItem>
     <FormItem label="Message">
       {getFieldDecorator('message', {
-        initialValue: Mention.toContentState('Message with mentions'),
+        initialValue: Mention.toContentState(
+          '56 minutes into the game, Zlatan Ibrahimovic scores! Manchester United leads 1-0.  #ChampionTheMatch and share this post to celebrate with us!',
+        ),
       })(<Mention style={{ width: '100%', height: 100 }} suggestions={[actor.name]} multiLines />)}
     </FormItem>
   </Form>
